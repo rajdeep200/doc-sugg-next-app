f9892417f7dd602ff7f117cc3cf1002b323fff50
Rajdeep Ghosh <rajdeep.ghosh@fedev.cbnits.com>
2025-06-22 14:39:15 +0530

    second commit

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import PdfViewerWrapper from './PdfViewerWrapper'
 import { useRouter } from 'next/navigation'
--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname, './')
+
         config.module.rules.push({
             test: /pdf\.worker(\.min)?\.js/,
             use: { loader: 'worker-loader' },
         })
+
         return config
     },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
